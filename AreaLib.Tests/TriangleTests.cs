08eae4edddd19d4af664ab56d4ab4ceb4850a292
Paul Pestov <pavel.pestov@gmail.com>
2023-03-17 17:36:46 +0400

    Added tests for "IsRight()".

---
+++ AreaLib.Tests/TriangleTests.cs
@@ -0,0 +1,22 @@
+﻿using AreaLib.Figures;
+
+namespace AreaLib.Tests;
+
+public class TriangleTests
+{
+    [Test]
+    [TestCase(3, 4, 5, true)]
+    [TestCase(3, 4, 6, false)]
+    public void TestIsRight(double a, double b, double c, bool expected)
+    {
+        // Arrange
+        var circle = new Triangle();
+        circle.SetParameters(a, b, c);
+
+        // Act
+        var result = circle.IsRight();
+
+        //Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
